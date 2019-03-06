3b7e21489e550e7e83ce5b90711d21218d351286
woody <ykworx@gmail.com>
2019-03-06 20:28:03 +0900

    bugfix

--- src/main.c
+++ src/main.c
@@ -7,5 +7,5 @@ int main(int argc, char *argv[])
   printf("MAJOR %d MINOR %d PATCH %d\n", APP_VERSION_MAJOR,
       APP_VERSION_MINOR,
       APP_VERSION_PATCH);
-  print("TEST 1");
+  printf("TEST 1");
 }
